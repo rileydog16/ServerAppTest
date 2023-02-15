cf8c5fe58a305f5fd3f50c8c8756c0cefcbe5a43
paulb <paulb@ashridgetech.com>
2023-02-15 12:34:51 +0000

    Get firebase config into client build

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,5 +8,6 @@ echo $OUTPUT_DIR
 
 curl --silent $TOOLS_URL/tools/tools.js > $SCRIPT_DIR/tools.js
 curl --silent $TOOLS_URL/tools/tools.js.map > $SCRIPT_DIR/tools.js.map
+./node_modules/.bin/firebase apps:sdkconfig WEB > $PROJECT_DIR/sdkConfig.out
 rm -rf $OUTPUT_DIR
 node $SCRIPT_DIR/build.js $PROJECT_DIR $OUTPUT_DIR $TOOLS_URL
