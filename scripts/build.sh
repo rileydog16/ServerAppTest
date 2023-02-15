9ef82d3fd27c0ee705ebd2d4d77943f0ab325e41
paulb <paulb@ashridgetech.com>
2023-02-15 12:57:50 +0000

    Print firebase debug log

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,5 +9,8 @@ echo $OUTPUT_DIR
 curl --silent $TOOLS_URL/tools/tools.js > $SCRIPT_DIR/tools.js
 curl --silent $TOOLS_URL/tools/tools.js.map > $SCRIPT_DIR/tools.js.map
 ./node_modules/.bin/firebase apps:sdkconfig WEB > $PROJECT_DIR/sdkConfig.out
+if test -e firebase-debug.log; then
+    cat firebase-debug.log
+fi
 rm -rf $OUTPUT_DIR
 node $SCRIPT_DIR/build.js $PROJECT_DIR $OUTPUT_DIR $TOOLS_URL
